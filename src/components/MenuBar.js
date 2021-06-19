bd129ae5355511974fbd4f9eecebc7f83e024718
Doug Perez <perezcpt@gmail.com>
2021-06-18 23:29:34 -0700

    clean up

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -33,57 +33,42 @@ const MenuBar = () => {
 
   return (
     <div>
-      <Animated
-        animationIn="fadeInUp"
-        animationOut="fadeOut"
-        isVisible={true}
-        className="titleFade"
-      >
-        <div className="button-container">
-          {/* <button className="frosted-glass">Home</button> */}
+      <div className="button-container">
+        {/* <button className="frosted-glass">Home</button> */}
 
-          <button className="frosted-glass" onClick={() => setState("about")}>
-            About Alex
-          </button>
-          {state === "about" ? <AboutAlex goHome={goHome} /> : null}
-          <button
-            className="frosted-glass"
-            onClick={() => setState("photoGallery")}
-          >
-            Photos
-          </button>
-          {state === "photoGallery" ? <PhotoGallery goHome={goHome} /> : null}
-          <button className="frosted-glass" onClick={() => setState("why")}>
-            Why
-          </button>
-          {state === "why" ? <KnowDrugs goHome={goHome} /> : null}
-          <button
-            className="frosted-glass"
-            onClick={() => setState("knowDrugs")}
-          >
-            Know Drugs
-          </button>
-          {state === "knowDrugs" ? <KnowDrugs goHome={goHome} /> : null}
-          <button
-            className="frosted-glass"
-            onClick={() => setState("youArePowerful")}
-          >
-            You Are Powerful
-          </button>
-          {state === "youArePowerful" ? (
-            <YouArePowerful goHome={goHome} />
-          ) : null}
-          <button
-            className="frosted-glass"
-            onClick={() => setState("resources")}
-          >
-            Resources
-          </button>
-          {state === "resources" ? <Resources goHome={goHome} /> : null}
+        <button className="frosted-glass" onClick={() => setState("about")}>
+          About Alex
+        </button>
+        {state === "about" ? <AboutAlex goHome={goHome} /> : null}
+        <button
+          className="frosted-glass"
+          onClick={() => setState("photoGallery")}
+        >
+          Photos
+        </button>
+        {state === "photoGallery" ? <PhotoGallery goHome={goHome} /> : null}
+        <button className="frosted-glass" onClick={() => setState("why")}>
+          Why
+        </button>
+        {state === "why" ? <KnowDrugs goHome={goHome} /> : null}
+        <button className="frosted-glass" onClick={() => setState("knowDrugs")}>
+          Know Drugs
+        </button>
+        {state === "knowDrugs" ? <KnowDrugs goHome={goHome} /> : null}
+        <button
+          className="frosted-glass"
+          onClick={() => setState("youArePowerful")}
+        >
+          You Are Powerful
+        </button>
+        {state === "youArePowerful" ? <YouArePowerful goHome={goHome} /> : null}
+        <button className="frosted-glass" onClick={() => setState("resources")}>
+          Resources
+        </button>
+        {state === "resources" ? <Resources goHome={goHome} /> : null}
 
-          <button className="frosted-glass">Blog</button>
-        </div>
-      </Animated>
+        <button className="frosted-glass">Blog</button>
+      </div>
     </div>
   );
 };
--- src/components/views/AboutAlex.js
+++ src/components/views/AboutAlex.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 import { Animated } from "react-animated-css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const AboutAlex = ({ goHome }) => {
   console.log("ABOUT");
@@ -10,6 +13,17 @@ const AboutAlex = ({ goHome }) => {
         <button className="back-btn" onClick={() => goHome()}>
           <FaTimes />
         </button>
+        <Container fluid>
+          <Row>
+            <Col sm={8}>sm=8</Col>
+            <Col sm={4}>sm=4</Col>
+          </Row>
+          <Row>
+            <Col sm>sm=true</Col>
+            <Col sm>sm=true</Col>
+            <Col sm>sm=true</Col>
+          </Row>
+        </Container>
       </div>
     </>
   );
