4a54495ebdc9d939eded3fd9d054c60c98d77bf4
Doug Perez <perezcpt@gmail.com>
2021-06-26 01:02:21 -0700

    text section added

--- src/components/views/AboutAlex.js
+++ src/components/views/AboutAlex.js
@@ -13,17 +13,59 @@ const AboutAlex = ({ goHome }) => {
         <button className="back-btn" onClick={() => goHome()}>
           <FaTimes />
         </button>
-        <Container fluid>
-          <Row>
-            <Col sm={8}>sm=8</Col>
-            <Col sm={4}>sm=4</Col>
-          </Row>
-          <Row>
-            <Col sm>sm=true</Col>
-            <Col sm>sm=true</Col>
-            <Col sm>sm=true</Col>
-          </Row>
-        </Container>
+        <div className="bodyText">
+          <div className="wrapper">
+            <h2 className="textH2">
+              About Alex <span className="nowrap">We Remember</span>
+            </h2>
+            <p>
+              Alex had a big and genuine heart, he loved his friends, nature
+              hikes on the mountain and beach, photography, fishing, basketball,
+              and good food. He was tri-lingual, enjoyed playing pool and long
+              walks with his dog. His favorite place was Maui. Alex suffered
+              from anxiety and began to self medicate which led to addiction and
+              his tragic, unexpected death from accidental Fentanyl poisoning.
+              He had planned a trip to Maui and Europe this year after
+              completing his detox regime. He wanted nothing more than have his
+              life back and his shining light will always be with those he
+              touched.
+            </p>
+            <span className="nowrap">Our Fondest Memories.</span>
+            <p className="justify">
+              His smile, big hugs, his surprises and letters, the way he enjoyed
+              exploring new places when traveling, finding the best pastries
+              when visiting Germany, his excitement when preparing trips, his
+              love for dogs, his enthusiasm about NBA basketball, and his
+              passion for boggie boarding, fishing and his attention to detail
+              for the equipment. Alex's tall 6'4 body, his beautiful eyes, his
+              vast human-being-ness.
+            </p>
+            <span className="nowrap">We will Never Forget.</span>
+            <p className="justify">
+              Alex' s presence, his charisma, his big and generous heart, his
+              vast insightfulness and striving to allow for everyone to be seen
+              for who they are, beyond stigma and judgement. His bear hugs, all
+              of them.
+            </p>
+            <span className="nowrap">Alex Will Always Be In Our Hears.</span>
+            <p className="justify">
+              Alex loved life. He believed in equality, fairness and seeing
+              through stigma and outer shell. He supported diversity and
+              believed in each human being having an important message. He was a
+              loyal friend, loved dogs, barbecues and fishing, and found refuge
+              in nature, his healing place. He always had something good to say
+              about all his friends, giving everyone a second chance. Alex
+              inspired people and taught them life skills. As his oldest friend
+              says, "Everybody needs to have a friend like Alex".
+            </p>
+            {/* <span className="nowrap">Körper dehnte.</span>
+            <p className="justify">
+              “YOU CAN TELL WHEN YOU ARE AT THE RIGHT PLACE BY THE WAY IT
+              FEELS.” — ALEX MOVAHEDI
+              <span className="nowrap">Körper dehnte.</span>
+            </p> */}
+          </div>
+        </div>
       </div>
     </>
   );
